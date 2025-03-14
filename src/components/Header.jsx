e6a0b723a31b34e017c2052fdb68b5bb0ba0d537
anikatasnin <jarintasnin09@gmail.com>
2025-03-14 23:11:42 +0600

    added dynamic date using moment.js

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
+import moment from "moment";
+
 const Header = () => {
   return (
-    <div className="flex flex-col justify-center items-center gap-2">
+    <div className="flex flex-col items-center gap-2">
       <h2 className="text-6xl font-unifrakturmaguntia">The New York Times</h2>
       <p className="text-[#706F6F] ">Journalism Without Fear or Favour</p>
       <p className="text-[#706F6F] font-medium">
-        <span className="text-[#403F3F]">Sunday,</span> November 27, 2025
+        <span className="text-[#403F3F]">
+          {moment().format("dddd, MMMM D, YYYY")}
+        </span>{" "}
       </p>
     </div>
   );
