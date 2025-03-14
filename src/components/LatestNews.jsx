8c40b0fc31983f0f716386d92683a5f4bd663f67
anikatasnin <jarintasnin09@gmail.com>
2025-03-14 23:21:17 +0600

    create a marquee

---
+++ src/components/LatestNews.jsx
@@ -0,0 +1,9 @@
+const LatestNews = () => {
+  return (
+    <div className="flex">
+      <p className="bg-[#D72050] text-base-100 px-3 py-1">Latest</p>
+    </div>
+  );
+};
+
+export default LatestNews;
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
+import LatestNews from "../components/LatestNews";
 
 const HomeLayout = () => {
   return (
     <header>
       <Header></Header>
+      <section className="w-11/12 mx-auto">
+        <LatestNews></LatestNews>
+      </section>
     </header>
   );
 };
