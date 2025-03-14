8c810b75049123ef40d678eca8ecdc985e00b6fb
anikatasnin <jarintasnin09@gmail.com>
2025-03-15 00:00:44 +0600

    added user icon and login button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import userIcon from "../assets/user.png";
 
 const Navbar = () => {
   return (
@@ -8,6 +9,12 @@ const Navbar = () => {
         <Link to="/">Home</Link>
         <Link to="/career">Career</Link>
         <Link to="/about">About</Link>
+      </div>
+      <div className="login">
+        <div className="flex gap-2">
+          <img src={userIcon} alt="" />
+          <button className="btn btn-neutral rounded-none">Login</button>
+        </div>
       </div>
     </div>
   );
