de3826f51a0721ce9246636787a5aa0f4611b6ac
anikatasnin <jarintasnin09@gmail.com>
2025-03-14 23:30:23 +0600

    create marquee link

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -1,11 +1,17 @@
+import Marquee from "react-fast-marquee";
+import { Link } from "react-router-dom";
+
 const LatestNews = () => {
   return (
     <div className="flex gap-2 items-center">
       <p className="bg-[#D72050] text-base-100 px-3 py-1">Latest</p>
-      <p>
-        Match Highlights: Germany vs Spain — as it happened ! Match Highlights:
-        Germany vs Spain as...
-      </p>
+      <Marquee>
+        <Link to="/news">
+          Match Highlights: Germany vs Spain — As it happened! ⚽🔥 | Final
+          Score: 2-1 | Top Scorers: Müller ⚽, Morata ⚽ | Next Match: France vs
+          Italy 🏆 Stay tuned for more updates!
+        </Link>
+      </Marquee>
     </div>
   );
 };
