bb6cb4e7a4965b31cc5bf8648f87192d8e3d2b34
anikatasnin <jarintasnin09@gmail.com>
2025-03-15 00:06:12 +0600

    create layout of main content

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -15,7 +15,7 @@ const HomeLayout = () => {
       <nav className="w-11/12 mx-auto">
         <Navbar></Navbar>
       </nav>
-      <main className="w-11/12 mx-auto pt-5 grid grid-cols-12">
+      <main className="w-11/12 mx-auto pt-5 grid md:grid-cols-12">
         <aside className="left col-span-3">Left</aside>
         <section className="col-span-6">main</section>
         <aside className="cols-span-3">right</aside>
