0c5a52a774c66125c740d8e20e53b0b02e2a1457
anikatasnin <jarintasnin09@gmail.com>
2025-03-14 22:19:37 +0600

    added header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-
 const Header = () => {
   return (
     <div>
-      <h2>Header</h2>
+      <h2 className="text-5xl text-red-500 font-unifrakturmaguntia">
+        The New York Times
+      </h2>
     </div>
   );
 };
