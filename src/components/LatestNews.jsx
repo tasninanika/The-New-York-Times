16d3860990fe742390d587643c6166ba65a89366
anikatasnin <jarintasnin09@gmail.com>
2025-03-14 23:23:57 +0600

    create a marquee

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -1,7 +1,11 @@
 const LatestNews = () => {
   return (
-    <div className="flex">
+    <div className="flex gap-2 items-center">
       <p className="bg-[#D72050] text-base-100 px-3 py-1">Latest</p>
+      <p>
+        Match Highlights: Germany vs Spain — as it happened ! Match Highlights:
+        Germany vs Spain as...
+      </p>
     </div>
   );
 };
