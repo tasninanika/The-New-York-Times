45296d6e1f9240557727b6dee2ea17b368817afc
anikatasnin <jarintasnin09@gmail.com>
2025-03-14 22:59:05 +0600

    added name, date

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 const Header = () => {
   return (
-    <div>
-      <h2 className="text-5xl text-red-500 font-unifrakturmaguntia">
-        The New York Times
-      </h2>
+    <div className="flex flex-col justify-center items-center gap-2">
+      <h2 className="text-6xl font-unifrakturmaguntia">The New York Times</h2>
+      <p className="text-[#706F6F] ">Journalism Without Fear or Favour</p>
+      <p className="text-[#706F6F] font-medium">
+        <span className="text-[#403F3F]">Sunday,</span> November 27, 2025
+      </p>
     </div>
   );
 };
