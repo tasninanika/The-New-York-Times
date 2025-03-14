fd2f516b1fedec1c62f5d4b7d8c7e72dc0a57523
anikatasnin <jarintasnin09@gmail.com>
2025-03-14 21:04:38 +0600

    create paths

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,7 +3,19 @@ import { createBrowserRouter } from "react-router-dom";
 const routes = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <div>Home</div>,
+  },
+  {
+    path: "news",
+    element: <div>News</div>,
+  },
+  {
+    path: "login",
+    element: <div>Login</div>,
+  },
+  {
+    path: "register",
+    element: <div>Register</div>,
   },
 ]);
 
