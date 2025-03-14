aed0917b3e9364c30cde497d88543e721e514975
anikatasnin <jarintasnin09@gmail.com>
2025-03-14 23:44:20 +0600

    create marquee pauseonhover

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -5,17 +5,17 @@ const LatestNews = () => {
   return (
     <div className="flex gap-4 items-center bg-base-200 py-3 px-3">
       <p className="bg-[#D72050] text-base-100 px-3 py-1">Latest</p>
-      <Marquee pauseOnHover={true} className="space-x-10">
+      <Marquee pauseOnHover={true} speed={80} className="space-x-10">
         <Link to="/news">
           Match Highlights: Germany vs Spain — As it happened! ⚽🔥 | Final
           Score: 2-1 | Top Scorers: Müller ⚽, Morata ⚽ | Next Match: France vs
           Italy 🏆 Stay tuned for more updates!
         </Link>
-        <Link to="/news">
+        <Link to="/news" className="ms-16">
           Breaking: Manchester United signs a new star player! ⚽🔥 Transfer
           deal worth €80M confirmed. Fans are excited for the upcoming season!
         </Link>
-        <Link to="/news">
+        <Link to="/news" className="ms-16">
           Champions League Thriller! 🏆 Real Madrid secures a last-minute
           victory against PSG with a stunning goal from Vinícius Jr. ⚡ Watch
           the highlights now!
