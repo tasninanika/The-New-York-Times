16ab786f2a9bd65faa10778f7466b694174f41d8
anikatasnin <jarintasnin09@gmail.com>
2025-03-15 23:22:45 +0600

    setup firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB4L1HDfVtNZYBeV5abW8vr9dvy2O_mL7A",
+  authDomain: "the-new-york-times-d3d7f.firebaseapp.com",
+  projectId: "the-new-york-times-d3d7f",
+  storageBucket: "the-new-york-times-d3d7f.firebasestorage.app",
+  messagingSenderId: "88712172129",
+  appId: "1:88712172129:web:ac4375b018e72c767d5020",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
