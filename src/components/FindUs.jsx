ae4f29f99bfa766ee7b446ee5b513e197607b606
anikatasnin <jarintasnin09@gmail.com>
2025-03-15 10:23:20 +0600

    Styled all social buttons in right navbar

--- src/components/FindUs.jsx
+++ src/components/FindUs.jsx
@@ -3,16 +3,16 @@ import { FaFacebook } from "react-icons/fa";
 
 const FindUs = () => {
   return (
-    <div>
-      <h2 className="font-semibold mb-3">Find Us On</h2>
-      <div className="join join-vertical flex *:w-88 *:bg-base-100">
-        <button className="btn join-item justify-start">
+    <div className="my-10">
+      <h2 className="font-semibold mb-3 text-[20px]">Find Us On</h2>
+      <div className="join join-vertical flex *:w-88 *:bg-base-100 *:py-7">
+        <button className="btn join-item justify-start hover:bg-base-300">
           <FaFacebook></FaFacebook> Facebook
         </button>
-        <button className="btn join-item justify-start">
+        <button className="btn join-item justify-start hover:bg-base-300">
           <BsTwitter></BsTwitter> Twitter
         </button>
-        <button className="btn join-item justify-start">
+        <button className="btn join-item justify-start hover:bg-base-300">
           <BsInstagram></BsInstagram> Instagram
         </button>
       </div>
--- src/components/SocialLogin.jsx
+++ src/components/SocialLogin.jsx
@@ -4,7 +4,7 @@ import { FaGithub } from "react-icons/fa";
 const SocialLogin = () => {
   return (
     <div>
-      <h2 className="font-semibold mb-3">Login With</h2>
+      <h2 className="font-semibold mb-3 text-[20px]">Login With</h2>
       <div className="*:w-88 space-y-2">
         <button className="btn">
           <FaGoogle /> Login with Google
