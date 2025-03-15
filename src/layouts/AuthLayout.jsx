aac89ff6613501ed0791c3d5ddc29cb2340ec4e0
anikatasnin <jarintasnin09@gmail.com>
2025-03-15 22:23:13 +0600

    added navbar in auth layout

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const AuthLayout = () => {
   return (
     <div>
+      <header className="py-3 w-11/12 mx-auto">
+        <Navbar></Navbar>
+      </header>
       <Outlet></Outlet>
     </div>
   );
