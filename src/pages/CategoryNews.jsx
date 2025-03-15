c7ad0057fe8419a26f3629b2d7b5b6a594666c25
anikatasnin <jarintasnin09@gmail.com>
2025-03-15 20:36:47 +0600

    added margin to the category news

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -7,7 +7,9 @@ const CategoryNews = () => {
 
   return (
     <div>
-      <h2 className="font-semibold mb-5">The New York Times Home</h2>
+      <h2 className="font-semibold mb-5 text-[20px] mx-10">
+        The New York Times Home
+      </h2>
       <div>
         {news.map((singleNews) => (
           <NewsCard key={singleNews._id} news={singleNews}></NewsCard>
