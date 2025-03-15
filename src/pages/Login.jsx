05781e0d8894199e45ab594418c54d108e07c220
anikatasnin <jarintasnin09@gmail.com>
2025-03-15 22:36:05 +0600

    styled login form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,18 +5,30 @@ const Login = () => {
         <h2 className="text-2xl font-semibold text-center">
           Login Your Account
         </h2>
-        <div className="card-body">
-          <fieldset className="fieldset">
-            <label className="fieldset-label">Email</label>
-            <input type="email" className="input" placeholder="Email" />
-            <label className="fieldset-label">Password</label>
-            <input type="password" className="input" placeholder="Password" />
-            <div>
-              <a className="link link-hover">Forgot password?</a>
-            </div>
-            <button className="btn btn-neutral mt-4">Login</button>
-          </fieldset>
-        </div>
+        <form>
+          <div className="card-body">
+            <fieldset className="fieldset">
+              <label className="fieldset-label">Email</label>
+              <input
+                type="email"
+                className="input bg-[#F3F3F3] w-full border-none"
+                placeholder="Enter your email address"
+              />
+              <label className="fieldset-label">Password</label>
+              <input
+                type="password"
+                className="input bg-[#F3F3F3] w-full border-none"
+                placeholder="Enter your password"
+              />
+              <div>
+                <a className="link link-hover">Forgot password?</a>
+              </div>
+              <button className="btn bg-[#403F3F] rounded-none mt-4 text-white">
+                Login
+              </button>
+            </fieldset>
+          </div>
+        </form>
       </div>
     </div>
   );
