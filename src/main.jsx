292880acb91e33a3c4b40fc16806f6a5b630d9c7
anikatasnin <jarintasnin09@gmail.com>
2025-03-15 23:32:19 +0600

    added router inside the auth provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import routes from "./Routes/Routes.jsx";
+import AuthProvider from "./provider/AuthProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={routes}></RouterProvider>
+    <AuthProvider>
+      <RouterProvider router={routes}></RouterProvider>
+    </AuthProvider>
   </StrictMode>
 );
