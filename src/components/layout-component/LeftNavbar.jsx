969c81a710f45487a0b69c253dfec2d8bbe8dcdb
anikatasnin <jarintasnin09@gmail.com>
2025-03-15 09:34:47 +0600

    state created

--- src/components/layout-component/LeftNavbar.jsx
+++ src/components/layout-component/LeftNavbar.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LeftNavbar = () => {
+  const [categories, setCategories] = useState([]);
+
   return (
     <div>
       <h2 className="text-lg font-semibold">All Category</h2>
