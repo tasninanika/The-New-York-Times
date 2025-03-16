b7a504549d3591a5e2764d8e3056a1a8b566226f
anikatasnin <jarintasnin09@gmail.com>
2025-03-16 19:31:00 +0600

    used onAuthStateChanged

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../provider/AuthProvider";
 
 const Register = () => {
-  const { createNewUser } = useContext(AuthContext);
+  const { createNewUser, user, setUser } = useContext(AuthContext);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,6 +18,7 @@ const Register = () => {
     createNewUser(email, password)
       .then((result) => {
         const user = result.user;
+        setUser(user);
         console.log(user);
       })
       .catch((error) => {
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,5 +1,9 @@
-import { createContext, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+} from "firebase/auth";
 import { app } from "../firebase/firebase.config";
 
 export const AuthContext = createContext();
@@ -17,6 +21,16 @@ const AuthProvider = ({ children }) => {
     setUser,
     createNewUser,
   };
+
+  useEffect(() => {
+    onAuthStateChanged(
+      auth,
+      (currentUser) => {
+        setUser(currentUser);
+      },
+      []
+    );
+  });
 
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
