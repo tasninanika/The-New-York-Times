1e37b4f3a5fda26b6bae9fa622d7ed04aa15d511
anikatasnin <jarintasnin09@gmail.com>
2025-03-16 20:07:14 +0600

    created news details page

--- src/pages/NewsDetails.jsx
+++ src/pages/NewsDetails.jsx
@@ -1,5 +1,20 @@
+import Header from "../components/Header";
+import RightNavbar from "../components/layout-component/RightNavbar";
+
 const NewsDetails = () => {
-  return <div>News details here</div>;
+  return (
+    <div>
+      <header>
+        <Header></Header>
+      </header>
+      <main className="w-11/12 mx-auto grid grid-cols-12 gap-5">
+        <section className="col-span-9"></section>
+        <aside className="col-span-3">
+          <RightNavbar></RightNavbar>
+        </aside>
+      </main>
+    </div>
+  );
 };
 
 export default NewsDetails;
