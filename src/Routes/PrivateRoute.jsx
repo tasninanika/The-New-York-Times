9cc0840cb0d84402733d44bdd9e638ce49297ee3
anikatasnin <jarintasnin09@gmail.com>
2025-03-16 21:58:47 +0600

    used location

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,10 +1,12 @@
 import { useContext } from "react";
 import { AuthContext } from "../provider/AuthProvider";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import Loading from "../pages/Loading";
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+  console.log(location);
   if (loading) {
     <Loading></Loading>;
   }
