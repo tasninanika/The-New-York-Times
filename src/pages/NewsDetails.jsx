1d4c75da311709bbc0d3983dd26f1e93e4aaf6cc
anikatasnin <jarintasnin09@gmail.com>
2025-03-16 20:15:08 +0600

    created card

--- src/pages/NewsDetails.jsx
+++ src/pages/NewsDetails.jsx
@@ -4,7 +4,8 @@ import RightNavbar from "../components/layout-component/RightNavbar";
 
 const NewsDetails = () => {
   const data = useLoaderData();
-  console.log(data);
+  const news = data.data[0];
+  console.log(news);
   return (
     <div>
       <header>
@@ -13,6 +14,23 @@ const NewsDetails = () => {
       <main className="w-11/12 mx-auto grid grid-cols-12 gap-5">
         <section className="col-span-9">
           <h2 className="font-semibold mb-3">The New York Times</h2>
+          <div className="card bg-base-100  shadow-xl">
+            <figure className="px-10 pt-10">
+              <img src={news?.image_url} alt="Shoes" className="rounded-xl" />
+            </figure>
+            <div className="card-body ">
+              <h2 className="card-title">{news?.title}</h2>
+              <p>{news?.details}</p>
+              <div className="card-actions">
+                <Link
+                  to={`/category/${news?.category_id}`}
+                  className="btn btn-primary"
+                >
+                  Back to Category
+                </Link>
+              </div>
+            </div>
+          </div>
         </section>
         <aside className="col-span-3">
           <RightNavbar></RightNavbar>
