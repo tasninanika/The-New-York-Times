dd3bde14edc69d89870f545c9ab78e60ddf8d1af
anikatasnin <jarintasnin09@gmail.com>
2025-03-16 21:02:22 +0600

    corrected news path

--- src/pages/NewsDetails.jsx
+++ src/pages/NewsDetails.jsx
@@ -1,9 +1,10 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import Header from "../components/Header";
 import RightNavbar from "../components/layout-component/RightNavbar";
 
 const NewsDetails = () => {
   const data = useLoaderData();
+  console.log(data);
   const news = data.data[0];
   console.log(news);
   return (
@@ -13,10 +14,10 @@ const NewsDetails = () => {
       </header>
       <main className="w-11/12 mx-auto grid grid-cols-12 gap-5">
         <section className="col-span-9">
-          <h2 className="font-semibold mb-3">The New York Times</h2>
+          <h2 className="font-semibold mb-3">Dragon News</h2>
           <div className="card bg-base-100  shadow-xl">
             <figure className="px-10 pt-10">
-              <img src={news?.image_url} alt="Shoes" className="rounded-xl" />
+              <img src={news?.image_url} alt="news" className="rounded-xl" />
             </figure>
             <div className="card-body ">
               <h2 className="card-title">{news?.title}</h2>
