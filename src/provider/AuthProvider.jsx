4027c5e118de5c3dccc6e742e9ccc0437e06ff7f
anikatasnin <jarintasnin09@gmail.com>
2025-03-16 19:06:39 +0600

    created create user with email and password

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState } from "react";
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
 
 export const AuthContext = createContext();
@@ -7,6 +7,10 @@ const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+
+  const createNewUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth);
+  };
 
   const authInfo = {
     user,
