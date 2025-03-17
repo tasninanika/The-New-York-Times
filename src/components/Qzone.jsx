ee10ef2350e2e261b0253a4d5d335ea59c259fe6
anikatasnin <jarintasnin09@gmail.com>
2025-03-17 20:07:42 +0600

    styled q-zone

--- src/components/Qzone.jsx
+++ src/components/Qzone.jsx
@@ -1,18 +1,24 @@
 import swimming from "../assets/swimming.png";
 import classImg from "../assets/class.png";
 import playGround from "../assets/playground.png";
+import bg from "../assets/bg.png";
 const Qzone = () => {
   return (
     <div>
-      <h2>Q-Zone</h2>
+      <div className="bg-[#F3F3F3] space-y-5 p-3 mb-5">
+        <h2 className="text-[#403F3F] text-[20px] font-bold">Q-Zone</h2>
+        <figure>
+          <img src={swimming} alt="" />
+        </figure>
+        <figure>
+          <img src={classImg} alt="" />
+        </figure>
+        <figure>
+          <img src={playGround} alt="" />
+        </figure>
+      </div>
       <figure>
-        <img src={swimming} alt="" />
-      </figure>
-      <figure>
-        <img src={classImg} alt="" />
-      </figure>
-      <figure>
-        <img src={playGround} alt="" />
+        <img src={bg} alt="" />
       </figure>
     </div>
   );
