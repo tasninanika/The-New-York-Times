16bd8172cc850f8e4ad79bbf062b3561278700ca
anikatasnin <jarintasnin09@gmail.com>
2025-03-17 10:26:38 +0600

    styled left navbar

--- src/components/SocialLogin.jsx
+++ src/components/SocialLogin.jsx
@@ -4,8 +4,8 @@ import { FaGithub } from "react-icons/fa";
 const SocialLogin = () => {
   return (
     <div>
-      <h2 className="font-semibold mb-3 text-[20px]">Login With</h2>
-      <div className="*:w-88 space-y-2">
+      <h2 className="font-semibold mb-5 text-[20px]">Login With</h2>
+      <div className="*:w-full space-y-2">
         <button className="btn">
           <FaGoogle /> Login with Google
         </button>
--- src/components/layout-component/LeftNavbar.jsx
+++ src/components/layout-component/LeftNavbar.jsx
@@ -12,13 +12,13 @@ const LeftNavbar = () => {
 
   return (
     <div>
-      <h2 className="text-lg font-semibold mb-5">All Category</h2>
+      <h2 className="text-[20px] font-semibold mb-5">All Category</h2>
       <div className="flex flex-col gap-3">
         {categories.map((category) => (
           <NavLink
             to={`/category/${category.category_id}`}
             key={category.category_id}
-            className="btn border-none bg-white shadow-none py-6 text-[#9F9F9F] flex items-center justify-start pl-10"
+            className="btn border-none bg-white shadow-none py-6 text-[#9F9F9F] flex items-center justify-start pl-10 hover:bg-base-300"
           >
             {category.category_name}
           </NavLink>
