d4ddd354d9441f3b911d013b231017d1506870a1
anikatasnin <jarintasnin09@gmail.com>
2025-03-17 21:11:41 +0600

    added sign in with google method

--- src/components/SocialLogin.jsx
+++ src/components/SocialLogin.jsx
@@ -5,10 +5,14 @@ import AuthProvider from "../provider/AuthProvider";
 
 const SocialLogin = () => {
   const { signInWithGoogle } = useContext(AuthProvider);
-  signInWithGoogle().then((result) => {
-    const user = result.user;
-    console.log(user);
-  });
+  signInWithGoogle()
+    .then((result) => {
+      const user = result.user;
+      console.log(user);
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
   return (
     <div>
       <h2 className="font-bold mb-5 text-[20px]">Login With</h2>
