2224814c8f442f528c6c9ff1c554e78c6012f45d
anikatasnin <jarintasnin09@gmail.com>
2025-03-17 19:40:31 +0600

    styled sports card

--- src/components/LeftSports.jsx
+++ src/components/LeftSports.jsx
@@ -2,15 +2,19 @@ const LeftSports = ({ sport }) => {
   const { author, title, image_url } = sport;
   return (
     <div>
-      <div className="card bg-base-100 w-96 shadow-sm">
+      <div className="card bg-base-100 shadow-sm">
         <figure>
-          <img src={image_url} alt="Shoes" />
+          <img
+            src={image_url}
+            alt="Shoes"
+            className="w-[267px] h-[150px] mb-3"
+          />
         </figure>
-        <div className="card-body">
+        <div className="">
           <h2 className="card-title">{title}</h2>
-          <div className="card-actions justify-between">
+          <div className="card-actions flex justify-between">
             <div className="badge badge-outline">Sports</div>
-            <div className="badge badge-outline">{author.published_date}</div>
+            <div className="text-sm text-[#9F9F9F]">Jan 4, 2025</div>
           </div>
         </div>
       </div>
